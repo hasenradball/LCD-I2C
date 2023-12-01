c569524945c2b866458ab36f7ab1479e1be3879a
Hasenradball <mail@frankhaefele.de>
2023-12-01 23:19:38 +0100

    save work

---
+++ examples/Hello_World/Hello_World.ino
@@ -0,0 +1,40 @@
+/*
+*	Hello_World.ino
+*
+*	Author: Frank Häfele
+*	Date:	01.12.2023
+*
+*	Object: Print Hello World on LCD Display
+*/
+
+#include <LCD-I2C.h>
+
+LCD-I2C lcd(0x27, 16, 2); // Default address of most PCF8574 modules, change according
+
+void setup()
+{
+    lcd.begin(); // If you are using more I2C devices using the Wire library use lcd.begin(false)
+                 // this stop the library(LCD-I2C) from calling Wire.begin()
+    lcd.backlight();
+}
+
+void loop()
+{
+    lcd.print("     Hello"); // You can make spaces using well... spaces
+    lcd.setCursor(5, 1); // Or setting the cursor in the desired position.
+    lcd.print("World!");
+    delay(500);
+
+    // Flashing the backlight
+    for (int i = 0; i < 5; ++i)
+    {
+        lcd.backlight();
+        delay(50);
+        lcd.noBacklight();
+        delay(50);
+    }
+
+    lcd.backlight();
+    lcd.clear();
+    delay(500);
+}
